c3e06586a020d229cc9ce0067e53e45365f4f675
Charlotte <81417641+Cuzozzi@users.noreply.github.com>
2022-06-27 10:37:30 +0200

    fixed data error

--- src/pages/account.js
+++ src/pages/account.js
@@ -2,18 +2,23 @@ import "../output.css";
 
 const axios = require("axios");
 
+function test() {
+  console.log("this works");
+}
+
 function Account() {
   return (
     <main>
       <h2>Account</h2>
       <button
         onClick={() => {
+          console.log("boop");
           axios({
             method: "post",
             url: "http://localhost:5433/user",
             data: {
               firstName: "Fred",
-              lastName: "Flintstone",
+              secondName: "Flintstone",
             },
           });
         }}
