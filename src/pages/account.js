32ed8979c512570e4d997e25aa54c3afdd050ac8
Charlotte <81417641+Cuzozzi@users.noreply.github.com>
2022-07-03 18:55:30 +0200

    removed redundant button

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,26 +1,9 @@
 import "../output.css";
 
-const axios = require("axios");
-
 function Account() {
   return (
     <main>
       <h2>Account</h2>
-      <button
-        onClick={() => {
-          console.log("boop");
-          axios({
-            method: "post",
-            url: "http://localhost:5433/user",
-            data: {
-              firstName: "Fred",
-              secondName: "Flintstone",
-            },
-          });
-        }}
-      >
-        Add User
-      </button>
       <p>Placeholder</p>
     </main>
   );
